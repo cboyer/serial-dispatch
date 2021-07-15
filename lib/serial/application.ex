44fcc84d120b3aed60a81205aa77d5d1ce2e3af5
cboyer <37932659+cboyer@users.noreply.github.com>
2021-07-15 18:32:24 -0400

    File writer modification

--- lib/serial/application.ex
+++ lib/serial/application.ex
@@ -1,51 +1,47 @@
 defmodule Serial.Application do
-  @moduledoc """
-  Main process in charge of supervision
-  """
-  use Application
-  use Supervisor
-  require Logger
+    @moduledoc """
+    Main process in charge of supervision
+    """
 
-  # Configuration parameters
-  @writer_output "/tmp/obd2.csv"    # Write data to file (append)
-  @writer_buf 10                    # Wait for 10 lines of data before writing to file
-  @watch_sleep 500                  # Monitor device availability every 500ms if disconnected
-  @timeout 1000                     # :timeout if no data is received after X ms
-  @listen_addr {127,0,0,1}          # Listen fro TCP connections on IP address
-  @tcp_port 8080                    # Listen for TCP connections on port 8080
-  @speed 115200                     # Serial speed
-  @line_separator "\n"              # End of line character
-  @print_data 1                     # Print received data in terminal
-  
-  # Device to look for
-  @device %{description: "Arduino Due", manufacturer: "Arduino LLC", product_id: 62, vendor_id: 9025} #Macchina M2
-  #@device %{product_id: 2010, vendor_id: 32903} #Bluetooth connection through rfcomm
-  #@device %{manufacturer: "Arduino (www.arduino.cc)", product_id: 67, serial_number: "9543335363635141E0F0", vendor_id: 9025} #Arduino Uno
+    use Application
+    use Supervisor
+    require Logger
 
 
-  # Supervisor needs a init/1 (if not defined: warning)
-  def init(_arg) do
-  end
+    # Configuration parameters
+    @writer_output "/tmp/obd2.csv"    # Write data to file (append)
+    @watch_sleep 500                  # Monitor device availability every 500ms if disconnected
+    @timeout 1000                     # :timeout if no data is received after X ms
+    @listen_addr {127,0,0,1}          # Listen for TCP connections on IP address
+    @tcp_port 8080                    # Listen for TCP connections on port 8080
+    @speed 115200                     # Serial speed
+    @line_separator "\n"              # End of line character
+    @print_data 1                     # Print received data in terminal
 
-  def start(_type, _args) do
-    Logger.info("[#{__MODULE__}] Started at #{inspect(self())}")
-    Logger.info("[#{__MODULE__}] Starting subprocesses...")
+    # Device to look for
+    @device %{description: "Arduino Due", manufacturer: "Arduino LLC", product_id: 62, vendor_id: 9025} #Macchina M2
+    #@device %{product_id: 2010, vendor_id: 32903} #Bluetooth connection through rfcomm
+    #@device %{manufacturer: "Arduino (www.arduino.cc)", product_id: 67, serial_number: "9543335363635141E0F0", vendor_id: 9025} #Arduino Uno
 
-    children = [
-      {Serial.Writer, [@writer_output, @writer_buf]},
-      {Serial.TcpServer, [@listen_addr, @tcp_port]},
-      {Serial.Listener, [@watch_sleep, @timeout, @device, @speed, @line_separator, @print_data]}
-    ]
 
-    opts = [strategy: :one_for_one, name: Serial.Supervisor]
-    {:ok, supervisor_pid} = Supervisor.start_link(children, opts)
+    # Supervisor needs a init/1 (if not defined: warning)
+    def init(_arg) do
+    end
 
-    Logger.info(
-      "[#{__MODULE__}] All subprocesses started: #{
-        inspect(Supervisor.which_children(supervisor_pid))
-      }"
-    )
+    def start(_type, _args) do
+        Logger.info("[#{__MODULE__}] Started at #{inspect(self())}")
+        Logger.info("[#{__MODULE__}] Starting subprocesses...")
 
-    {:ok, supervisor_pid}
-  end
+        children = [
+            {Serial.Writer, [@writer_output]},
+            {Serial.TcpServer, [@listen_addr, @tcp_port]},
+            {Serial.Listener, [@watch_sleep, @timeout, @device, @speed, @line_separator, @print_data]}
+        ]
+
+        opts = [strategy: :one_for_one, name: Serial.Supervisor]
+        {:ok, supervisor_pid} = Supervisor.start_link(children, opts)
+
+        Logger.info("[#{__MODULE__}] All subprocesses started: #{inspect(Supervisor.which_children(supervisor_pid))}")
+        {:ok, supervisor_pid}
+    end
 end
--- lib/serial/writer.ex
+++ lib/serial/writer.ex
@@ -1,45 +1,49 @@
 defmodule Serial.Writer do
-  @moduledoc """
-  Serial.Writer writes data from serial device to file
-  """
-  use GenServer
-  require Logger
+    @moduledoc """
+    Serial.Writer writes data from serial device to file
+    """
 
-  @doc """
-  Default start_link function: args come from parent (Supervisor)
-  """
-  def start_link(args) do
-    state = %{
-      writer_output: args |> Enum.at(0) |> File.open!([:append, :binary]),
-      writer_buf:    args |> Enum.at(1)
-    }
+    use GenServer
+    require Logger
 
-    {:ok, supervisor_pid} = GenServer.start_link(__MODULE__, state, name: __MODULE__)
-    Logger.info("[#{__MODULE__}] Started at #{inspect(supervisor_pid)}")
-    {:ok, supervisor_pid}
-  end
 
-  @doc """
-  Default init function (executed after start_link/1)
-  Parameter "state" provided with GenServer.start_link from start_link/1
-  """
-  def init(state) do
-    {:ok, state}
-  end
+    @doc """
+    Default start_link function: args come from parent (Supervisor)
+    """
+    def start_link(args) do
+        state = %{
+            writer_output: args |> Enum.at(0) |> File.open!([:append, :binary])
+        }
 
-  @doc """
-  Message handler
-  """
-  def handle_info(data, state) do
-    case data do
-      {:partial, msg} ->
-        Logger.info("[#{__MODULE__}] Received partial: #{inspect(msg)}")
-
-      msg ->
-        state.writer_output
-        |> IO.write(msg <> "\n")
+        {:ok, supervisor_pid} = GenServer.start_link(__MODULE__, state, name: __MODULE__)
+        Logger.info("[#{__MODULE__}] Started at #{inspect(supervisor_pid)}")
+        {:ok, supervisor_pid}
     end
 
-    {:noreply, state}
-  end
+
+    @doc """
+    Default init function (executed after start_link/1)
+    Parameter "state" provided with GenServer.start_link from start_link/1
+    """
+    def init(state) do
+        {:ok, state}
+    end
+
+
+    @doc """
+    Message handler
+    """
+    def handle_info(data, state) do
+        case data do
+            {:partial, msg} -> 
+                Logger.info("[#{__MODULE__}] Received partial: #{inspect(msg)}")
+
+            msg ->
+                spawn(fn -> state.writer_output
+                            |> IO.write(msg <> "\n")
+                end)
+        end
+
+        {:noreply, state}
+    end
 end
